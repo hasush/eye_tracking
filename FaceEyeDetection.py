d43e14eaf52499d3c4f13a4e5884183e228db441
Yash Sandhu <gsandh16@ford.com>
2018-07-31 10:36:29 -0400

    Object oriented code update.

--- FaceEyeDetection.py
+++ FaceEyeDetection.py
@@ -2,13 +2,18 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-scaleFactor = 1.01
-minNeighbors = 5
+scaleFactorFaces = 1.01
+minNeighborsFaces = 4 #4
+
+scaleFactorEyes = 1.2 #1.2/5=301 or 1.1=291, 1.3=251, 1.25=245, 1.09=277, 1.21=293, 1.19=292
+minNeighborsEyes = 5 #with 1.2: 4=298, 3=281, 2=256, 6 = 300, 7 =293
+
 faceRectangleColor = (255,0,0)
 faceRectangleThickness = 2
 eyeRectangleColor = (0,255,0)
 eyeRectangleThickness = 2
 opencvDataPath = '/home/gsandh16/pythonEnvs/eye_tracking/lib/python3.6/site-packages/cv2/data/'
+cropFaces = True
 
 class FaceEyeDetection(object):
 	""" Class for face and eye detection """
@@ -19,80 +24,119 @@ class FaceEyeDetection(object):
 		# Load the Haar-cascade based face and eye classifiers.
 		self.face_cascade = cv2.CascadeClassifier(opencvDataPath + 'haarcascade_frontalface_default.xml')
 		self.eye_cascade = cv2.CascadeClassifier(opencvDataPath + 'haarcascade_eye.xml')
-		self.faces = []
-		self.eyes = []
-		self.faceRoiGray = []
-		self.faceRoiColor = []
+		self.facesCoords = []
+		self.eyesCoords = []
+		self.facesRoiGray = []
+		self.facesRoiColor = []
+		self.eyesRoiGray = []
+		self.eyesRoiColor = []
 		self.image = None
 		self.imageGray = None
-		self.faceImages = None
-		self.eyeImages = None
 
 	def loadImageFromDisk(self, filePath):
 		""" Load an iamge from disk and set to a member variable.
 			@param filePath File path of the image that will be read.
 		"""
 
+		print(filePath)
+
 		# Read the image.
 		self.image = cv2.imread(filePath)
 
-		print(type(self.image))
-		print(self.image.shape)
+		# plt.imshow(self.image)
+		# plt.show()
 
-		# self.image = self.image[150:, 400:800, :]
+		# print(type(self.image))
+		# print(self.image.shape)
+
+		if cropsFaces:
+			self.image = self.image[150:, 400:800, :]
 
 	def convertBgrImageToGray(self):
 		""" Convert an BGR image to a gray scale image """
 
 		# Convert the image.
 		self.imageGray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
-
-		plt.imshow(self.imageGray)
-		plt.show()
 		
 	def detectFaces(self):
 		""" Function to detect face given that it exists on disk. """
 
 		# Detect faces within the image.
-		self.faces = self.face_cascade.detectMultiScale(self.imageGray, scaleFactor, minNeighbors)
+		self.facesCoords = self.face_cascade.detectMultiScale(self.imageGray, scaleFactorFaces, minNeighborsFaces)
 
-		assert len(self.faces) > 0
+		# Check to make sure a face was detected.
+		if len(self.facesCoords) < 1:
+			return False
+		else:
+			return True
 
-	def detectEyesInFaces(self):
+	def detectEyesInFace(self):
+		""" Detect eyes contained in the faces """
+
+		# Check to make sure only one face.
+		# assert len(self.facesCoords) == 1
+		if len(self.facesCoords) < 1:
+			return
+
+		# Reset the face ROIs and eyes.
+		self.eyesCoords = []
+		self.facesRoiGray = []
+		self.facesRoiColor = []
+
+		# Obtain coords of the face.
+		(x,y,w,h) = self.facesCoords[0]
+
+		# Get ROIs of the face.
+		self.facesRoiGray.append(self.imageGray[y:y+h, x:x+w])
+		self.facesRoiColor.append(self.image[y:y+h, x:x+w])
+
+		# Find the eyes of the current ROI.
+		self.eyesCoords.append(self.eye_cascade.detectMultiScale(self.facesRoiGray[0], scaleFactorEyes, minNeighborsEyes))
+
+		# Make sure only one set of eyes and that the set contains two eyes.
+		if len(self.eyesCoords) != 1 or len(self.eyesCoords[0]) != 2:
+			self.facesRoiGray.pop()
+			self.facesRoiColor.pop()
+			return
+
+		assert len(self.eyesCoords) == 1
+		assert len(self.eyesCoords[0]) == 2
+
+		# Loop over coordinates of the eyes.
+		for (ex, ey, ew, eh) in self.eyesCoords[0]:
+
+			# Get the ROIs of the eyes.
+			self.eyesRoiGray.append(self.imageGray[y+ey:y+ey+eh,x+ex:x+ex+ew])
+			self.eyesRoiColor.append(self.image[y+ey:y+ey+eh,x+ex:x+ex+ew])
+
+	def drawRectanglesOnEyesAndFace(self):
 		""" Detect eyes contained in the faces """
 
 		# Reset the face ROIs and eyes.
-		self.eyes = []
-		self.faceRoiGray = []
-		self.faceRoiColor = []
+		self.eyesCoords = []
+		self.facesRoiGray = []
+		self.facesRoiColor = []
 
 		# Loop over the coordinates of the faces.
-		for (x,y,w,h) in self.faces:
+		for (x,y,w,h) in self.facesCoords:
 
-
-			plt.imshow(self.image[y:y+h, x:x+w])
-			plt.show()
-			
 			# Draw a rectangle on the face.
 			cv2.rectangle(self.image, (x, y), (x+w, y+h), faceRectangleColor, faceRectangleThickness)
 
-			
-
-
-			# Get ROIs of the images.
-			self.faceRoiGray.append(self.imageGray[y:y+h, x:x+w])
-			self.faceRoiColor.append(self.image[y:y+h, x:x+w])
+			# Get ROIs of the face.
+			self.facesRoiGray.append(self.imageGray[y:y+h, x:x+w])
+			self.facesRoiColor.append(self.image[y:y+h, x:x+w])
 
 			# Find the eyes of the current ROI.
-			self.eyes.append(self.eye_cascade.detectMultiScale(self.faceRoiGray[-1]))
+			self.eyesCoords.append(self.eye_cascade.detectMultiScale(self.facesRoiGray[-1], scaleFactorEyes, minNeighborsEyes))
 
-			assert len(self.eyes) > 0
+			assert len(self.eyesCoords) == 1
 
-			# Draw rectangles for the eyes.
-			for (ex, ey, ew, eh) in self.eyes[-1]:
+			# Loop over coordinates of the eyes.
+			for (ex, ey, ew, eh) in self.eyesCoords[-1]:
 
 				# Draw rectangles on the eyes.
-				cv2.rectangle(self.faceRoiColor[-1], (ex, ey), (ex+ew, ey+eh), eyeRectangleColor, eyeRectangleThickness)
+				cv2.rectangle(self.facesRoiColor[-1], (ex, ey), (ex+ew, ey+eh), eyeRectangleColor, eyeRectangleThickness)
 
 	def detectAndDrawRectanglesOnFaces(self, filePath):
 		""" Draw rectangles on faces and eyes within an image contained
@@ -102,24 +146,58 @@ class FaceEyeDetection(object):
 		self.loadImageFromDisk(filePath)
 		self.convertBgrImageToGray()
 		self.detectFaces()
-		self.detectEyesInFaces()
+		self.drawRectanglesOnEyesAndFace()
 
 		plt.imshow(self.image)
 		plt.show()
-		# cv2.imshow('image', self.image)
-		# cv2.waitKey(0)
-		# cv2.destroyAllWindows()
 
+	def detectFaceAndEyes(self):
+		self.detectFaces()
+		self.detectEyesInFace()
 
+	def determineNumFacesEyes(self, filePath):
 
+		# Read the image.
+		self.image = cv2.imread(filePath)
 
+		# Convert the image.
+		self.imageGray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
 
+		# Detect faces within the image.
+		self.facesCoords = self.face_cascade.detectMultiScale(self.imageGray, scaleFactorFaces, minNeighborsFaces)
 
+		# Check to make sure one face was detected.
+		if len(self.facesCoords) != 1:
+			return False
 
+		# Obtain coords of the face.
+		(x,y,w,h) = self.facesCoords[0]
 
+		# Get ROIs of the face.
+		self.facesRoiGray = self.imageGray[y:y+h, x:x+w]
+		self.facesRoiColor = self.image[y:y+h, x:x+w]
 
+		# Find the eyes of the current ROI.
+		self.eyesCoords = self.eye_cascade.detectMultiScale(self.facesRoiGray, scaleFactorEyes, minNeighborsEyes)
 
+		# Make sure only one set of eyes and that the set contains two eyes.
+		if len(self.eyesCoords) != 2:
+			return False
+		else:
+			return True
 
+		# Coordinates of the first eye.
+		(ex1, ey1, ew1, eh1) = self.eyesCoords[0]
 
+		# Extrat images of the first eye.
+		self.eyesRoiGray1 = self.imageGray[y+ey1:y+ey1+eh1,x+ex1:x+ex1+ew1]
+		self.eyesRoiColor1 = self.image[y+ey1:y+ey1+eh1,x+ex1:x+ex1+ew1]
 
-		
+		# Coordinates of the second eye.
+		(ex2, ey2, ew2, eh2) = self.eyesCoords[1]
+
+		# Extract images of the second eye.
+		self.eyesRoiGray2 = self.imageGray[y+ey2:y+ey2+eh2,x+ex2:x+ex2+ew2]
+		self.eyesRoiColor2 = self.image[y+ey2:y+ey2+eh2,x+ex2:x+ex2+ew2]
+
+		return True
--- main.py
+++ main.py
@@ -1,15 +1,111 @@
+import os
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
 from FaceEyeDetection import FaceEyeDetection
+
+imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/unrealSyntheticImages/run2/00000.png'
+# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/einstein.jpg'
+# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/mario.jpg'
+# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/lena.jpeg'
+# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/marilyn.jpg'
+
+# Path to directory containing images whose faces and eyes will be extracted.
+imageDirectory = '/home/gsandh16/Documents/gazeTracking/data/unrealSyntheticImages/run2/'
+
+def determineAverageFaceEyeSize(imageDirectoryFilePath):
+	""" Loop over all images within the directory and run facial and eye detection in
+		order to determine the average size window for face and eye detection.
+
+		@param imageDirectoryFilePath Path to the directory where images are contained.
+	"""
+
+	# Instantiate the face/eye detector.
+	faceEyeDetection = FaceEyeDetection()
+
+	# Obtain the contents of the directory.
+	directoryList = os.listdir(imageDirectoryFilePath)
+
+	# Containers to hold the dimensions of the images.
+	eyeDimensions = []
+	faceDimensions = []
+
+	numberMatchingParams = 0
+	numberNotMatchingParams = 0
+
+	# Loop over the contents of the directory.
+	for index, file in enumerate(directoryList[0:1000]):
+
+		if index % 20 == 0:
+			print("Iteration: ", index)
+
+		# If the file is the json file which contains gaze information, continue.
+		if '.json' in file:
+			continue
+		else:
+
+			# Concatenate the file path.
+			imageFilePath = imageDirectoryFilePath + file
+
+			# Find the face and eyes in the image.
+			foundFacesEyes = faceEyeDetection.determineNumFacesEyes(imageFilePath)
+
+			if foundFacesEyes:
+				numberMatchingParams +=1
+			elif not foundFacesEyes:
+				numberNotMatchingParams +=1
+
+
+	print("Matching: ", numberMatchingParams)
+	print("Not Matching: ", numberNotMatchingParams)
+
+
+
 
 def main():
 
-	# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/unrealSyntheticImages/run2/00000.png'
-	imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/einstein.jpg'
-	# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/mario.jpg'
-	# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/lena.jpeg'
-	# imageFilePath = '/home/gsandh16/Documents/gazeTracking/data/marilyn.jpg'
+	
+
+	
 
 	faceEyeDetection = FaceEyeDetection()
-	faceEyeDetection.detectAndDrawRectanglesOnFaces(imageFilePath)
+
+	# # Find the face and eyes in the image.
+	# foundFacesEyes = faceEyeDetection.detectionPipeLine(imageFilePath)
+
+	# print(foundFacesEyes)
+
+	# sys.exit()
+
+	# faceEyeDetection.loadImageFromDisk(imageFilePath)
+	# faceEyeDetection.convertBgrImageToGray()
+	# faceEyeDetection.detectFaceAndEyes()
+
+	# plt.figure()
+	# plt.imshow(faceEyeDetection.facesRoiGray[0], cmap='gray')
+	# plt.figure()
+	# plt.imshow(faceEyeDetection.eyesRoiGray[0], cmap='gray')
+	# plt.figure()
+	# plt.imshow(faceEyeDetection.eyesRoiGray[1], cmap='gray')
+	# plt.show()
+
+	# faceEyeDetection.detectAndDrawRectanglesOnFaces(imageFilePath)
+
+
+	
+
+	determineAverageFaceEyeSize(imageDirectory)
+
+	
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
 	main()
